5f7d7d5be3971ee3ee2274ad5fc7c6bac9282448
Lea Edwards <leaxedw@gmail.com>
2026-02-17 20:16:07 -0600

    added functions for posting new categories

--- json-server.py
+++ json-server.py
@@ -4,7 +4,7 @@ from nss_handler import HandleRequests, status
 
 from views import create_user, login_user, get_all_users, get_single_user
 from views import create_post, get_all_posts, get_single_users_post
-from views import get_all_categories
+from views import create_category, get_all_categories
 
 class JSONServer(HandleRequests):
     """Server class to handle incoming HTTP requests for shipping ships"""
@@ -91,6 +91,11 @@ class JSONServer(HandleRequests):
             else:
                 # If no user found, return a 400 or 401
                 return self.response("Invalid email", status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value)
+            
+        if resource == "categories":
+            response_json = create_category(request_body)
+            return self.response(response_json, status.HTTP_201_SUCCESS_CREATED.value)
+
                     
 
         return self.response(
--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,3 @@
 from .user import create_user, login_user, get_all_users, get_single_user
 from .post import create_post, get_all_posts, get_single_users_post
-from .category import get_all_categories
+from .category import create_category, get_all_categories
