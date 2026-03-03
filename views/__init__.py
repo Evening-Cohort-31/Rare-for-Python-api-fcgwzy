5becf6a920bb1b7121db72df80221a2bafb3ff02
Erin Telfer <eringtelfer@gmail.com>
2026-03-02 18:35:56 -0600

    fixed init imports

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,5 @@
-from .user import create_user, login_user, get_all_users
+from .user import create_user, login_user, get_all_users, user_is_admin
 from .category import create_category, get_all_categories, delete_category, update_category, get_single_category
-from .post import create_post, get_all_posts, get_single_users_post, get_post_details, update_post_tags
+from .post import create_post, get_all_posts, get_single_users_post, get_post_details, update_post_tags, delete_post
 from .comment import create_comment, get_all_comments
-from .tag import create_tag, get_all_tags
+from .tag import create_tag, get_all_tags, delete_tag
