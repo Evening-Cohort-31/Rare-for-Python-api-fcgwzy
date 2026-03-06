990a26dced19bc3050a093ebd6864f8535ba245e
Cade Ryan McCaskill <cademccaskill@gmail.com>
2026-03-05 21:35:19 -0600

    fixing a bug

--- json-server.py
+++ json-server.py
@@ -9,7 +9,7 @@ from views import (
     update_category,
     get_single_category,
 )
-from views import create_user, login_user, get_all_users, user_is_admin
+from views import create_user, login_user, get_all_users, get_user_by_id, user_is_admin
 from views import create_tag, get_all_tags, delete_tag
 from views import (
     create_post,
@@ -19,7 +19,6 @@ from views import (
     delete_post,
     update_post_tags,
     edit_post
-    
 )
 from views import create_comment, get_all_comments
 
@@ -31,23 +30,24 @@ class JSONServer(HandleRequests):
         query_params = url.get("query_params", {})
 
         if url["requested_resource"].lower() == "users":
+            if url["pk"] != 0:
+                response_body = get_user_by_id(url["pk"])
+                return self.response(response_body, status.HTTP_200_SUCCESS.value)
+
             response_body = get_all_users(query_params)
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
         if url["requested_resource"].lower() == "posts":
-            # Look for both variations of the key
             user_id = query_params.get("user_id") or query_params.get("userId")
 
             if user_id:
                 response_body = get_single_users_post(user_id)
                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
-            # Only treat as /posts/<id> if no user_id filter is present
             if url["pk"] != 0:
                 response_body = get_post_details(url["pk"])
                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
-            # Otherwise, return all
             response_body = get_all_posts()
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
@@ -67,19 +67,16 @@ class JSONServer(HandleRequests):
             response_body = get_all_tags()
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
-        
         return self.response(
-                "Resource not found",
-                status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
-            )
+            "Resource not found",
+            status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+        )
 
     def do_PUT(self):
         """Handle PUT requests from clients"""
         url = self.parse_url(self.path)
         pk = url.get("pk")
         resource = url.get("requested_resource").lower()
-
-        # Handle any "undefined" strings sent by React to avoid server crash
 
         if pk == "undefined":
             return self.response("ID in URL is undefined", status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA)
@@ -88,9 +85,7 @@ class JSONServer(HandleRequests):
         raw_body = self.rfile.read(content_len)
         request_body = json.loads(raw_body)
 
-
         if resource == "posts" and pk != 0:
-
             success = edit_post(pk, request_body)
 
             tag_ids = request_body.get("tag_ids", [])
@@ -218,215 +213,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# class JSONServer(HandleRequests):
-
-#     def do_GET(self):
-#         """Handle GET requests from a client"""
-#         url = self.parse_url(self.path)
-
-#         response_body = ""
-
-#         query_params = url.get("query_params", {})
-
-#         if url["requested_resource"].lower() == "users":
-#             if url["pk"] != 0:
-#                 # Gets the requested order by the id
-#                 response_body = get_all_users(query_params)
-#                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#             response_body = get_all_users(query_params)
-#             return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#         elif url["requested_resource"].lower() == "posts":
-
-#             # If there are query params, handle them first
-#             if len(query_params) > 0:
-
-#                 if "user_id" in query_params:
-#                     response_body = get_single_users_post(query_params["user_id"])
-#                     return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#             # Only treat as /posts/<id> if there are NO query params
-#             if url["pk"] != 0 and len(query_params) == 0:
-#                 response_body = get_post_details(url["pk"])
-#                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#             # Default: all posts
-#             response_body = get_all_posts()
-#             return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#         elif url["requested_resource"].lower() == "categories":
-#             if url["pk"] != 0:
-#                 # Gets the requested order by the id
-#                 response_body = get_all_categories()
-#                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#             response_body = get_all_categories()
-#             return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#         elif url["requested_resource"].lower() == "comments":
-#             if url["pk"] != 0:
-#                 # Gets the requested order by the id
-#                 response_body = get_all_comments()
-#                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#             response_body = get_all_comments()
-#             return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#         elif url["requested_resource"].lower() == "tags":
-#             if url["pk"] != 0:
-#                 response_body = get_all_tags()
-#                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#             response_body = get_all_tags()
-#             return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-#         else:
-#             return self.response(
-#                 "Resource not found",
-#                 status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
-#             )
-
-#     def do_PUT(self):
-#         """Handle PUT requests from clients"""
-#         url = self.parse_url(self.path)
-#         pk = url.get("pk")
-#         resource = url.get("requested_resource").lower()
-
-#         # Handle the "undefined" string sent by React to avoid server crash
-
-#         if pk == "undefined":
-#             return self.response("ID in URL is undefined", status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA)
-
-#         content_len = int(self.headers.get("content-length", 0))
-#         request_body = json.loads(self.rfile.read(content_len))
-
-#         if resource == "posts" and pk != 0:
-#             # 1. Update the Post itself
-#             success = edit_post(pk, request_body)
-
-#             # 2. Update the Tags
-#             tag_ids = request_body.get("tag_ids", [])
-#             update_post_tags(pk, tag_ids)
-
-#             if success:
-#                 return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
-#             return self.response("Post not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
-
-
-#         if url["requested_resource"].lower() == "categories" and url["pk"] != 0:
-
-#             content_len = int(self.headers.get("content-length", 0))
-#             request_body = self.rfile.read(content_len)
-#             request_body = json.loads(request_body)
-
-#             update_category(url["pk"], request_body)
-
-#             return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
-
-#         return self.response(
-#                 "Resource not found",
-#                 status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
-#             )
-
-#     def do_DELETE(self):
-#         """Handle the delete requests from clients"""
-#         url = self.parse_url(self.path)
-#         pk = url["pk"]
-
-#         if url["requested_resource"].lower() == "posts":
-#             if pk != 0:
-#                 delete_post(pk)
-#                 return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
-
-#         elif url["requested_resource"].lower() == "categories":
-#             if pk != 0:
-#                 successfully_deleted = delete_category(pk)
-#                 if successfully_deleted:
-#                     return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
-#                 return self.response(
-#                     "Resource not found",
-#                     status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
-#                 )
-
-#         elif url["requested_resource"].lower() == "tags":
-#             if pk == 0:
-#                 return self.response("Tag ID required", 400)
-
-#             auth_header = self.headers.get("Authorization")
-#             if not auth_header:
-#                 return self.response("Unauthorized", 401)
-
-#             try:
-#                 token = auth_header.split(" ")[1]
-#             except IndexError:
-#                 return self.response("Invalid Authorization Header", 401)
-
-#             if not user_is_admin(token):
-#                 return self.response("Forbidden: Admins only", 403)
-
-#             success = delete_tag(pk)
-#             if success:
-#                 return self.response("", 204)
-#             else:
-#                 return self.response("Not Found", 404)
-
-#         return self.response(
-#             "Resource not found",
-#             status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
-#         )
-
-#     def do_POST(self):
-#         """Handles POST request from client"""
-#         url = self.parse_url(self.path)
-
-#         content_len = int(self.headers.get("content-length", 0))
-#         request_body = self.rfile.read(content_len)
-#         request_body = json.loads(request_body)
-
-#         resource = url["requested_resource"]
-
-#         if resource == "register":
-#             response_json = create_user(request_body)
-#             return self.response(response_json, status.HTTP_201_SUCCESS_CREATED.value)
-
-#         if resource == "login":
-#             authenticated_user = login_user(request_body)
-#             if authenticated_user:
-#                 return self.response(authenticated_user, status.HTTP_200_SUCCESS.value)
-#             else:
-#                 return self.response(
-#                     "Invalid email", status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value
-#                 )
-
-#         if resource == "categories":
-#             response_json = create_category(request_body)
-#             return self.response(response_json, status.HTTP_201_SUCCESS_CREATED.value)
-
-#         if resource == "comments":
-#             response_json = create_comment(request_body)
-#             return self.response(response_json, status.HTTP_201_SUCCESS_CREATED.value)
-
-#         if resource == "tags":
-#             response_json = create_tag(request_body)
-#             return self.response(response_json, status.HTTP_201_SUCCESS_CREATED.value)
-
-#         if resource == "posts":
-#             response_json = create_post(request_body)
-#             return self.response(response_json, status.HTTP_201_SUCCESS_CREATED.value)
-
-#         return self.response(
-#             "Requested resource not found",
-#             status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
-#         )
-
-
-# def main():
-#     host = ""
-#     port = 8088
-#     HTTPServer((host, port), JSONServer).serve_forever()
-
-
-# if __name__ == "__main__":
-#     main()
--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,4 @@
-from .user import create_user, login_user, get_all_users, user_is_admin
+from .user import create_user, login_user, get_all_users, get_user_by_id, user_is_admin
 from .category import create_category, get_all_categories, delete_category, update_category, get_single_category
 from .tag import create_tag, get_all_tags, delete_tag
 from .post import (
